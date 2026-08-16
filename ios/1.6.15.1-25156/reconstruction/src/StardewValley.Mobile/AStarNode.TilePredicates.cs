7f586bef60a509a7fc888127998c9c6a0fb995a0
S. Ali Zaidi <ali@zaidi.fm>
2026-08-16 16:28:41 -0700

    Reconstruct initial TileClear child predicates

---
+++ ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/AStarNode.TilePredicates.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using StardewValley.Locations;
+
+namespace StardewValley.Mobile;
+
+public partial class AStarNode
+{
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public bool isTilePassable()
+	{
+		return TapToMoveUtils.IsTilePassable(_aStarGraph.gameLocation, x, y);
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public bool ContainsTravellingCart()
+	{
+		GameLocation location = _aStarGraph.gameLocation;
+		if (location == null || location.GetType() != typeof(Forest))
+			return false;
+
+		Forest forest = (Forest)location;
+		if (forest.travelingMerchantBounds == null)
+			return false;
+
+		Rectangle tileBounds = new Rectangle(x << 6, y << 6, 64, 64);
+		foreach (Rectangle merchantBounds in forest.travelingMerchantBounds)
+		{
+			if (merchantBounds.Intersects(tileBounds))
+				return true;
+		}
+
+		return false;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public bool ContainsTravellingDesertShop()
+	{
+		GameLocation location = _aStarGraph.gameLocation;
+		if (location == null || location.GetType() != typeof(Desert))
+			return false;
+
+		Rectangle tileBounds = new Rectangle(x << 6, y << 6, 64, 64);
+		return ((Desert)location).desertMerchantBounds.Intersects(tileBounds);
+	}
+}
