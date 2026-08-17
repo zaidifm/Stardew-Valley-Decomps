8649b73a6fe52c7ec6dc5fc180a4bb2a4b5c556a
S. Ali Zaidi <ali@zaidi.fm>
2026-08-16 17:52:18 -0700

    Reconstruct NPC tile predicates

--- ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/AStarNode.CharacterPredicates.cs
+++ ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/AStarNode.CharacterPredicates.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
+using StardewValley.Characters;
 using StardewValley.Locations;
 
 namespace StardewValley.Mobile;
@@ -21,5 +22,72 @@ public partial class AStarNode
 		}
 
 		return false;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public bool ContainsNPC()
+	{
+		GameLocation location = _aStarGraph.gameLocation;
+
+		if (location is Beach beach && beach.oldMariner != null)
+		{
+			Point standingPixel = beach.oldMariner.StandingPixel;
+			if (standingPixel.X / 64 == x && standingPixel.Y / 64 == y)
+				return true;
+		}
+
+		foreach (NPC npc in location.characters)
+		{
+			if (npc is Pet pet && pet.isSleepingOnFarmerBed.Value)
+				continue;
+
+			Point standingPixel = npc.StandingPixel;
+			if (standingPixel.X / 64 == x && standingPixel.Y / 64 == y)
+				return true;
+		}
+
+		if (location.currentEvent?.actors != null)
+		{
+			foreach (NPC actor in location.currentEvent.actors)
+			{
+				Point standingPixel = actor.StandingPixel;
+				if (standingPixel.X / 64 == x && standingPixel.Y / 64 == y)
+					return true;
+			}
+		}
+
+		return false;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public NPC FetchNPC()
+	{
+		GameLocation location = _aStarGraph.gameLocation;
+
+		if (location is Beach beach && beach.oldMariner != null)
+		{
+			Point standingPixel = beach.oldMariner.StandingPixel;
+			if (standingPixel.X / 64 == x && standingPixel.Y / 64 == y)
+				return beach.oldMariner;
+		}
+
+		foreach (NPC npc in location.characters)
+		{
+			Point standingPixel = npc.StandingPixel;
+			if (standingPixel.X / 64 == x && standingPixel.Y / 64 == y)
+				return npc;
+		}
+
+		if (location.currentEvent?.actors != null)
+		{
+			foreach (NPC actor in location.currentEvent.actors)
+			{
+				Point standingPixel = actor.StandingPixel;
+				if (standingPixel.X / 64 == x && standingPixel.Y / 64 == y)
+					return actor;
+			}
+		}
+
+		return null;
 	}
 }
