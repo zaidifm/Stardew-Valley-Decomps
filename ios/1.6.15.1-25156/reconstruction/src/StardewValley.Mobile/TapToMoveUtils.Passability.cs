441d9055f33833fad1b1591e0df4cb66eda4130f
S. Ali Zaidi <ali@zaidi.fm>
2026-08-17 08:12:34 -0700

    Expand TapToMoveUtils passability core

--- ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/TapToMoveUtils.Passability.cs
+++ ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/TapToMoveUtils.Passability.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
 using StardewValley.Locations;
 using xTile.Dimensions;
 using xTile.ObjectModel;
@@ -8,6 +9,53 @@ namespace StardewValley.Mobile;
 
 public partial class TapToMoveUtils
 {
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public static bool IsWater(Vector2 tile)
+	{
+		if (gameLocation is Submarine
+			&& tile.X >= 9f && tile.X <= 20f
+			&& tile.Y >= 7f && tile.Y <= 11f)
+		{
+			return true;
+		}
+
+		if (gameLocation is VolcanoDungeon volcanoDungeon)
+		{
+			if (volcanoDungeon.IsCooledLava((int)tile.X, (int)tile.Y))
+				return false;
+			if (volcanoDungeon.CanRefillWateringCanOnTile((int)tile.X, (int)tile.Y))
+				return true;
+		}
+
+		if (gameLocation.doesTileHaveProperty((int)tile.X, (int)tile.Y, "Water", "Back", false) != null)
+			return true;
+		return gameLocation.doesTileHaveProperty((int)tile.X, (int)tile.Y, "WaterSource", "Back", false) != null;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public static bool IsBuildingPassable(Vector2 tilePosition)
+	{
+		Tile tile = gameLocation.map.GetLayer("Buildings")
+			.PickTile(new Location((int)tilePosition.X << 6, (int)tilePosition.Y << 6), Game1.viewport.Size);
+		if (tile == null)
+			return false;
+
+		if (tile.TileIndexProperties.TryGetValue("Passable", out PropertyValue passable))
+		{
+			if (passable != null && passable.ToString() == "T")
+				return true;
+			if (passable.ToString() == "True")
+				return true;
+		}
+
+		tile.Properties.TryGetValue("Passable", out PropertyValue directPassable);
+		if (directPassable != null)
+			return true;
+
+		tile.TileIndexProperties.TryGetValue("Shadow", out PropertyValue shadow);
+		return shadow != null;
+	}
+
 	[MethodImpl(MethodImplOptions.NoInlining)]
 	public static bool IsTilePassable(GameLocation gameLocation, int tileX, int tileY)
 	{
@@ -42,13 +90,13 @@ public partial class TapToMoveUtils
 		}
 
 		tile.TileIndexProperties.TryGetValue("Passable", out PropertyValue tileIndexPassable);
-		PropertyValue directPassable = null;
-		PropertyValue shadow = null;
+		PropertyValue directPassable2 = null;
+		PropertyValue shadow2 = null;
 		if (tileIndexPassable == null)
 		{
-			tile.Properties.TryGetValue("Passable", out directPassable);
-			if (directPassable == null)
-				tile.TileIndexProperties.TryGetValue("Shadow", out shadow);
+			tile.Properties.TryGetValue("Passable", out directPassable2);
+			if (directPassable2 == null)
+				tile.TileIndexProperties.TryGetValue("Shadow", out shadow2);
 		}
 
 		if (tileIndexPassable != null
@@ -57,12 +105,12 @@ public partial class TapToMoveUtils
 			return true;
 		}
 
-		if (directPassable != null
-			&& directPassable.ToString().ToLower()[0] == 't')
+		if (directPassable2 != null
+			&& directPassable2.ToString().ToLower()[0] == 't')
 		{
 			return true;
 		}
 
-		return shadow != null;
+		return shadow2 != null;
 	}
 }
