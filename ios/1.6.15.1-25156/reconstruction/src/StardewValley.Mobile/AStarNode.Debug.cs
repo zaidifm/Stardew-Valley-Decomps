beee4b954359a267fddf752051ffb4892c9011c2
S. Ali Zaidi <ali@zaidi.fm>
2026-08-16 19:32:53 -0700

    Reconstruct object debug logger

--- ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/AStarNode.Debug.cs
+++ ios/1.6.15.1-25156/reconstruction/src/StardewValley.Mobile/AStarNode.Debug.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
 
 namespace StardewValley.Mobile;
 
@@ -9,5 +10,18 @@ public partial class AStarNode
 	{
 		DebugObjectParentSheetIndexOnTile();
 		_ = TileClear;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	public void DebugObjectParentSheetIndexOnTile()
+	{
+		if (_aStarGraph.gameLocation.objects.TryGetValue(new Vector2(x, y), out Object value))
+		{
+			Log.It(string.Concat(
+				"obj.parentSheetIndex:",
+				value.parentSheetIndex?.ToString(),
+				", ",
+				value.ToString()));
+		}
 	}
 }
